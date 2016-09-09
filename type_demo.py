5f3446951c92b0b910c1a80557dd50edb817c01c
anajera10 <anajera1@babson.edu>
2016-09-08 21:14:49 -0400

    In Class Session 3

---
+++ type_demo.py
@@ -0,0 +1,18 @@
+import time
+print(time.time())
+current=time.time()
+seconds=current//60 
+minutes = (current//60) % 60
+hours = (current//60)//60 %24
+days = current//60//60//24
+print('Current time: %d days, %d hours, %d minutes and %d seconds from Epoch.' % (days,hours,minutes,seconds))
+
+
+
+
+
+
+
+
+
+
